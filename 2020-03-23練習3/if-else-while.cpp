e370a9ec577cde00af7791bfca2a8ca5e2eca713
e96031413 <30921855+e96031413@users.noreply.github.com>
2020-03-23 10:02:33 +0800

    Add files via upload

---
+++ 2020-03-23練習3/if-else-while.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int num;
+	while (true){
+	printf("\ninput your number:");
+	scanf("%d", &num);
+	if ( num % 2 == 1 ){
+		printf("Yanwei Liu");
+	}
+	if (num % 2 == 0){
+		printf("410571040");
+	}
+	
+	}
+	return 0;
+}
